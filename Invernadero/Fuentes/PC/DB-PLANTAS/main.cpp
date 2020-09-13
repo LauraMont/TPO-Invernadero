8001af31d325361378ce16c541e196f88ad17f7a
Nicolás Taurozzi <ntaurozzi@est.frba.utn.edu.ar>
2020-09-13 10:55:24 -0300

    Cambios en la lista de tareas

--- Invernadero/Fuentes/PC/DB-PLANTAS/main.cpp
+++ Invernadero/Fuentes/PC/DB-PLANTAS/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *argv[])
    Observaciones:
    *Adaptar los datos (variables y campos que usaremos para las plantas) a nuestro proyecto -Pendiente: Ana
    *Adaptar el programa al tamaño que tenga -Hecho: Javier
-   *Embellecer la funcion de modificar datos -Pendiente: Nico T
+   *Embellecer la funcion de modificar datos -Hecho: Nico T
    *Cambiar la ubicacion de la base de datos, preferiblemente dentro de una carpeta (crear todo si no existe)  -Hecho: Nico T
    *Ir agregando prototipos de plantas -Pendiente: Nico P
 **/
